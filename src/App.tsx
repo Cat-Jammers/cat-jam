e203ceca193ee0a5482446e2f208e4f3fe3e2202
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-02-20 08:54:36 -0500

    Init vite

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+export function App() {
+  return (<><h1>Welcome to Cat Jam !! :3</h1>
+    <p>We are a team of 6 people on <a href="https://hackclub.com/slack/" target="_blank">Hack Club Slack</a> trying to
+      get socks for <a href="https://hackclub.slack.com/archives/C08DPRYMWF8" target="_blank">#sock</a>!! Try joining us!
+    </p>
+    <p>Stay tuned for our awesome project that still isn't decided as my other mates are 12:30 hours behind me :P</p>
+  
+    <div className="webring">
+      <a href="https://hackclub.slack.com/team/U07AZFQLPQ8" target="_blank">
+        <img src="https://ca.slack-edge.com/T0266FRGM-U07AZFQLPQ8-fc1c1ed8ba36-512" alt="KittyCat" />
+        <p>KittyCat</p>
+      </a>
+      <a href="https://hackclub.slack.com/team/U0266FRGP" target="_blank">
+        <img src="https://ca.slack-edge.com/T0266FRGM-U0266FRGP-0e1376df084b-512" alt="zrl" />
+        <p>zrl</p>
+      </a>
+      <a href="https://hackclub.slack.com/team/U078J6H1XL3" target="_blank">
+        <img src="https://ca.slack-edge.com/T0266FRGM-U078J6H1XL3-eb76cc600228-512" alt="phthallo" />
+        <p>phthallo</p>
+      </a>
+      <a href="https://hackclub.slack.com/team/U082RFNDVKQ" target="_blank">
+        <img src="https://ca.slack-edge.com/T0266FRGM-U082RFNDVKQ-g74b68349225-512" alt="DJ_Laser" />
+        <p>DJ_Laser</p>
+      </a>
+      <a href="https://hackclub.slack.com/team/U0829HRSQ76" target="_blank">
+        <img src="https://ca.slack-edge.com/T0266FRGM-U0829HRSQ76-e6aff3d33468-512" alt="PianoMan0" />
+        <p>PianoMan0</p>
+      </a>
+      <a href="https://hackclub.slack.com/team/U07FEFY2DNC" target="_blank">
+        <img src="https://ca.slack-edge.com/T0266FRGM-U07FEFY2DNC-d223a7106aa4-512" alt="crabby605" />
+        <p>crabby605</p>
+      </a>
+    </div>
+    <p>anyways thanks for visiting us if you see that the webapge has loaded b4 the images thats cos of cloudflare magic!
+    </p>
+    <p>pls pardon my css im bad at it :P</p></>)
+}
